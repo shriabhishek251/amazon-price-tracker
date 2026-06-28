551e512468e2edee591deef55d94f9ed5569d654
shriabhishek251 <abhi.k.shri.251@gmail.com>
2026-06-29 00:27:00 +0530

    debugging

--- main.py
+++ main.py
@@ -13,30 +13,33 @@ url = os.getenv("PRODUCT_URL")
 
 # ====================== Add Headers to the Request ===========================
 
-# header = {
-#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-#     "Accept-Encoding": "gzip, deflate, br, zstd",
-#     "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
-#     "Dnt": "1",
-#     "Priority": "u=1",
-#     "Sec-Fetch-Dest": "document",
-#     "Sec-Fetch-Mode": "navigate",
-#     "Sec-Fetch-Site": "none",
-#     "Sec-Fetch-User": "?1",
-#     "Sec-Gpc": "1",
-#     "Upgrade-Insecure-Requests": "1",
-#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
-# }
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
+    "Dnt": "1",
+    "Priority": "u=1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+}
 
 # A minimal header:
 
-header = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
-}
+# header = {
+#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+#     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+# }
 
 # Adding headers to the request
 response = requests.get(url, headers=header)
+
+print("Status Code:", response.status_code)
+print(response.text[:1000])      # First 1000 characters
 
 soup = BeautifulSoup(response.content, "html.parser")
 
@@ -53,7 +56,7 @@ title = soup.find(id="productTitle").get_text().strip()
 print(title)
 
 # Set the price below which you would like to get a notification
-BUY_PRICE = float(os.getenv["BUY_PRICE"])
+BUY_PRICE = float(os.getenv("BUY_PRICE"))
 
 if price_as_float < BUY_PRICE:
     message = f"{title} is on sale for {price}!"
@@ -62,9 +65,9 @@ if price_as_float < BUY_PRICE:
 
     with smtplib.SMTP(os.getenv["SMTP_ADDRESS"], port=587) as connection:
         connection.starttls()
-        result = connection.login(os.getenv["EMAIL_ADDRESS"], os.getenv["EMAIL_PASSWORD"])
+        result = connection.login(os.getenv("EMAIL_ADDRESS"), os.getenv("EMAIL_PASSWORD"))
         connection.sendmail(
-            from_addr=os.getenv["EMAIL_ADDRESS"],
-            to_addrs=os.getenv["EMAIL_ADDRESS"],
+            from_addr=os.getenv("EMAIL_ADDRESS"),
+            to_addrs=os.getenv("EMAIL_ADDRESS"),
             msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
         )
