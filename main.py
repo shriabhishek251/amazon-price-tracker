f8dd02604c7adb559a4cb93aea694e907e171f2f
shriabhishek251 <abhi.k.shri.251@gmail.com>
2026-06-28 23:51:22 +0530

    used os.getenv

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #URL of the product
-url = "https://www.amazon.in/Amkette-Brightness-Laser-Etched-Multi-Media-Connection/dp/B0CPM2QQ5B/ref=sr_1_1?crid=3S7Y76MBK5ZGP&dib=eyJ2IjoiMSJ9.r3d1h94ts-7bLH7zPC-U7B8FdoNk1dbZMNHcRb-yQmqIgzzz6o_5nd374O-i7itwhCKAs5j9gTfNvjpwOihSv5QmI_xTNqQ3osA_FKvLkFtmnBPPMHXhd0GjDWAmS6cQZknaJ03AY8HPJy8lzGFVOrjhzwSgoasqhDU7IIgNA-GHzqF9umdCfzS9zOjDBxH6uaqHX2Z6aYer4UdKVEwzg90DhoUbX96xARKvxmpzuyO7ra0bqMYnzLJbvrppfZ2uIetZZAbOVwlNtyzs4sttjx1xJkwRZQ5W3JpdZXcRjiM.KHGtT42bwNX0d8lMrSCLDTpiyFAkAqMkJDyCdgNB3hs&dib_tag=se&keywords=black%2Bkeyboard%2Bwith%2Bwhite%2Blight&qid=1782668459&s=electronics&sprefix=black%2Bkeyboard%2Bwith%2Bwhite%2Bligh%2Celectronics%2C273&sr=1-1&th=1"
+url = os.getenv("PRODUCT_URL")
+
 
 # ====================== Add Headers to the Request ===========================
 
@@ -52,18 +53,18 @@ title = soup.find(id="productTitle").get_text().strip()
 print(title)
 
 # Set the price below which you would like to get a notification
-BUY_PRICE = 800
+BUY_PRICE = float(os.getenv["BUY_PRICE"])
 
 if price_as_float < BUY_PRICE:
     message = f"{title} is on sale for {price}!"
 
     # ====================== Send the email ===========================
 
-    with smtplib.SMTP(os.environ["SMTP_ADDRESS"], port=587) as connection:
+    with smtplib.SMTP(os.getenv["SMTP_ADDRESS"], port=587) as connection:
         connection.starttls()
-        result = connection.login(os.environ["EMAIL_ADDRESS"], os.environ["EMAIL_PASSWORD"])
+        result = connection.login(os.getenv["EMAIL_ADDRESS"], os.getenv["EMAIL_PASSWORD"])
         connection.sendmail(
-            from_addr=os.environ["EMAIL_ADDRESS"],
-            to_addrs=os.environ["EMAIL_ADDRESS"],
+            from_addr=os.getenv["EMAIL_ADDRESS"],
+            to_addrs=os.getenv["EMAIL_ADDRESS"],
             msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
         )
